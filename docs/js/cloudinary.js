7a4f635d4705d765a40413a5810f441f09d9027e
Persephone007 <persephone007@gmail.com>
2020-02-13 14:21:25 -0500

    create docs for launch

---
+++ docs/js/cloudinary.js
@@ -0,0 +1,18 @@
+<button id="upload_widget" class="cloudinary-button">Upload files</button>
+
+<script src="https://widget.cloudinary.com/v2.0/global/all.js" type="text/javascript"></script>  
+
+<script type="text/javascript">  
+var myWidget = cloudinary.createUploadWidget({
+  cloudName: 'my_cloud_name', 
+  uploadPreset: 'my_preset'}, (error, result) => { 
+    if (!error && result && result.event === "success") { 
+      console.log('Done! Here is the image info: ', result.info); 
+    }
+  }
+)
+
+document.getElementById("upload_widget").addEventListener("click", function(){
+    myWidget.open();
+  }, false);
+</script>
